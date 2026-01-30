37a53d4f5a0e3a0e9d379f07e7005d4d9d97720f
go2665 <go2665@outlook.com>
2026-01-30 17:49:20 +0900

    초당 이동으로 변경

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
+import { clamp } from "./utils/math";
+
 const width = 1280;
 const height = 720;
 const knight = document.getElementById('knight') as HTMLImageElement;
 const knightSize = 256;
 let knightX = 0, knightY = 0;
-const step = 5;
+const moveSpeed = 200;
 
 // 어떤 키가 눌려있는지 저장하는 객체
 const keyStates: {[key: string]: boolean} = {};
@@ -16,34 +18,46 @@ window.addEventListener("keyup", (event) => {
   keyStates[event.key] = false;
 });
 
-function updateKnightPos() {
-  knight.style.left = `${knightX}px`;
-  knight.style.top = `${knightY}px`;
+function moveKnightPos(xDelta:number, yDelta:number) {
+    knightX = clamp(knightX + xDelta, 0, width - knightSize);
+    knightY = clamp(knightY + yDelta, 0, height - knightSize);
+    //knight.style.left = `${knightX}px`;
+    //knight.style.top = `${knightY}px`;
+    knight.style.transform = `translate(${knightX}px, ${knightY}px)`;
 }
 
 // 매 프레임마다 기사 이동
-function loop() {
-  let moved = false;
-  if (keyStates['ArrowUp']) {
-    knightY = Math.max(knightY - step, 0);
-    moved = true;
-  }
-  if (keyStates['ArrowDown']) {
-    knightY = Math.min(knightY + step, height - knightSize);
-    moved = true;
-  }
-  if (keyStates['ArrowLeft']) {
-    knightX = Math.max(knightX - step, 0);
-    moved = true;
-  }
-  if (keyStates['ArrowRight']) {
-    knightX = Math.min(knightX + step, width - knightSize);
-    moved = true;
-  }
-  if (moved) updateKnightPos();
-  requestAnimationFrame(loop);
+let last = performance.now();
+function loop(now = performance.now()) {
+    const deltaTime = (now - last) / 1000;
+    let moved = false;
+    let directionX = 0;
+    let directionY = 0;
+    if (keyStates['ArrowUp']) {
+        directionY -= 1;
+        moved = true;
+    }
+    if (keyStates['ArrowDown']) {
+        directionY += 1;
+        moved = true;
+    }
+    if (keyStates['ArrowLeft']) {
+        directionX -= 1;
+        moved = true;
+    }
+    if (keyStates['ArrowRight']) {
+        directionX += 1;
+        moved = true;
+    }
+    if (moved) {
+        let deltaMove = moveSpeed * deltaTime;
+        moveKnightPos(directionX*deltaMove, directionY*deltaMove);
+    }
+
+    last = now;
+    requestAnimationFrame(loop);
 }
 
 // 처음 위치 지정, 루프 시작
-updateKnightPos();
+moveKnightPos(0,0);
 requestAnimationFrame(loop);
